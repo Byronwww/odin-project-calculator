1631f72e3b13c039da5173fe0fb52fbfbde74019
Byron <byronwmit@gmail.com>
2023-01-24 18:22:48 +0000

    added addition, subtraction, multiplication and division functions

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+const a = 10;
+const b = 10
+
+function addition(a, b){
+    return a + b;
+}
+
+function subtraction(a, b){
+    return a - b;
+}
+
+function multiplication (a,b){
+    return a * b;
+}
+
+function division (a,b){
+    return a / b;
+}
