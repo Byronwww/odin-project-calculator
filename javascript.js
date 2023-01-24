009eefac31a01a864d81cf1c1bc8c5c60279b2fd
Byron <byronwmit@gmail.com>
2023-01-24 18:37:37 +0000

    Add Linter Adding function documentation Added operator function to act on inputs

--- javascript.js
+++ javascript.js
@@ -1,18 +1,54 @@
-const a = 10;
-const b = 10
 
-function addition(a, b){
-    return a + b;
+/**
+ * @param {int} a
+ * @param {int} b
+ * @return {int} a + b
+ */
+function addition(a, b) {
+  return a + b;
 }
 
-function subtraction(a, b){
-    return a - b;
+/**
+ * @param {int} a
+ * @param {int} b
+ * @return {int} a - b
+ */
+function subtraction(a, b) {
+  return a - b;
 }
 
-function multiplication (a,b){
-    return a * b;
+/**
+ * @param {int} a
+ * @param {int} b
+ * @return {int} a * b
+ */
+function multiplication(a, b) {
+  return a * b;
 }
 
-function division (a,b){
-    return a / b;
+/**
+ * @param {int} a
+ * @param {int} b
+ * @return {int} a / b
+ */
+function division(a, b) {
+  return a / b;
+}
+
+/**
+ * @param {*} a
+ * @param {*} b
+ * @param {*} operator (+, -, * , /)
+ * takes two numbers and an operator to do a sum
+ */
+function operate(a, b, operator) {
+  if (operator == '+') {
+    addition(a, b);
+  } else if (operator == '-') {
+    subtraction(a, b);
+  } else if (operator == '*') {
+    multiplication(a, b);
+  } else if (operator == '/') {
+    division(a, b);
+  }
 }
