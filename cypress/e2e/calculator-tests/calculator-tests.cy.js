32f8f8b3e2b92d2d8b65c4d4ac73c07dc5b884ad
Byron <byronwmit@gmail.com>
2023-06-20 16:09:33 +0100

    Added Cypress
    Added basic check that buttons display

---
+++ cypress/e2e/calculator-tests/calculator-tests.cy.js
@@ -0,0 +1,32 @@
+describe('example to-do app', () => {
+  beforeEach(() => {
+    cy.visit('../../../index.html')
+  })
+  
+  it('displays all calculator buttons', () => {
+    // bottom row
+    cy.get('#button0').should('have.text', '0')
+    cy.get('#buttonDot').should('have.text', ' . ')
+    cy.get('#buttonEquals').should('have.text', '=')
+    cy.get('#buttonAddition').should('have.text', '+')
+
+    //second from bottom row
+    cy.get('#button1').should('have.text', '1')
+    cy.get('#button2').should('have.text', '2')
+    cy.get('#button3').should('have.text', '3')
+    cy.get('#buttonSubtraction').should('have.text', '-')
+
+    //third from bottom row
+    cy.get('#button4').should('have.text', '4')
+    cy.get('#button5').should('have.text', '5')
+    cy.get('#button6').should('have.text', '6')
+    cy.get('#buttonMultiplication').should('have.text', 'x')
+
+    //top row
+    cy.get('#button7').should('have.text', '7')
+    cy.get('#button8').should('have.text', '8')
+    cy.get('#button9').should('have.text', '9')
+    cy.get('#buttonDivision').should('have.text', '÷')
+    cy.get('#buttonClear').should('have.text', 'C')
+  })
+})
