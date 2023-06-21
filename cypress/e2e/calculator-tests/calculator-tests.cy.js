046c9b80b4d0822e1649f8e924de6cc0353a376b
Byron <byronwmit@gmail.com>
2023-06-21 13:23:37 +0100

    add additional tests and early draft test strategy

--- cypress/e2e/calculator-tests/calculator-tests.cy.js
+++ cypress/e2e/calculator-tests/calculator-tests.cy.js
@@ -3,7 +3,7 @@ describe('calculator app', () => {
     cy.visit('../../../index.html')
   })
   
-  it('displays all calculator buttons', () => {
+  it('all calculator buttons are correctly labelled', () => {
     // bottom row
     cy.get('#button0').should('have.text', '0')
     cy.get('#buttonDot').should('have.text', ' . ')
@@ -28,6 +28,49 @@ describe('calculator app', () => {
     cy.get('#button9').should('have.text', '9')
     cy.get('#buttonDivision').should('have.text', '÷')
     cy.get('#buttonClear').should('have.text', 'C')
+  })
+
+  it('has a display', () => {
+    cy.get('#display').exists
+  })
+
+  it('each number shows on screen when the corresponding button is clicked and the C button clears the display', () => {
+    cy.get('#button1').click()
+    cy.get('#display').should('have.text' , '1')
+    cy.get('#buttonClear').click()
+    cy.get('#display').should('have.text' , '0')
+    cy.get('#button2').click()
+    cy.get('#display').should('have.text' , '2')
+    cy.get('#buttonClear').click()
+    cy.get('#display').should('have.text' , '0')
+    cy.get('#button3').click()
+    cy.get('#display').should('have.text' , '3')
+    cy.get('#buttonClear').click()
+    cy.get('#display').should('have.text' , '0')
+    cy.get('#button5').click()
+    cy.get('#display').should('have.text' , '5')
+    cy.get('#buttonClear').click()
+    cy.get('#display').should('have.text' , '0')
+    cy.get('#button6').click()
+    cy.get('#display').should('have.text' , '6')
+    cy.get('#buttonClear').click()
+    cy.get('#display').should('have.text' , '0')
+    cy.get('#button7').click()
+    cy.get('#display').should('have.text' , '7')
+    cy.get('#buttonClear').click()
+    cy.get('#display').should('have.text' , '0')
+    cy.get('#button8').click()
+    cy.get('#display').should('have.text' , '8')
+    cy.get('#buttonClear').click()
+    cy.get('#display').should('have.text' , '0')
+    cy.get('#button9').click()
+    cy.get('#display').should('have.text' , '9')
+    cy.get('#buttonClear').click()
+    cy.get('#display').should('have.text' , '0')
+    cy.get('#button0').click()
+    cy.get('#display').should('have.text' , '0')
+    cy.get('#buttonClear').click()
+    cy.get('#display').should('have.text' , '0')
   })
   
   it('basic calculations: 1 + 1 = 2' , () => {
