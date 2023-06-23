99b95df0137ad94b1300810800287b5cbfa1ed9c
Byron <byronwmit@gmail.com>
2023-06-23 12:54:13 +0100

    Add test to check that numbers do not start with zero if zero is pressed first

--- cypress/e2e/calculator-tests/calculator-tests.cy.js
+++ cypress/e2e/calculator-tests/calculator-tests.cy.js
@@ -373,5 +373,11 @@ describe('calculator app', () => {
     cy.get('#display').should('have.text', '3')
     cy.get('#buttonEquals').click()
     cy.get('#display').should('have.text', '3')
-  })  
+  })
+
+  it.only('0 Button Functionality - 0 Button does not add a zero to the front of numbers', () =>{
+    cy.get('#button0').click()
+    cy.get('#button1').click()
+    cy.get('#display').should('have.text', '1')
+  })
 })
